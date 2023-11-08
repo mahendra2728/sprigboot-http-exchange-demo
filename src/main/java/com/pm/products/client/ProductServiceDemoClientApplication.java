f172fec742557d5f28a4a6921cdef1b5b2f4e610
mahendra <mahendramaske28@gmail.com>
2023-11-08 17:12:21 +0530

    first commit

---
+++ src/main/java/com/pm/products/client/ProductServiceDemoClientApplication.java
@@ -0,0 +1,13 @@
+package com.pm.products.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceDemoClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceDemoClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pm/products/client/ProductServiceDemoClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.products.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceDemoClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
